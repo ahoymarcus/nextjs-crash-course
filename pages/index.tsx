9552cdbdec030b637ac8116039b73629a955093f
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2022-01-13 21:32:53 -0300

    Importar dependencia SASS. Criar e importar os modulos de estilo com scss ao projeto.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
 // https://www.youtube.com/watch?v=tt3PUvhOVzo
-//  0 hr  53'  10''
+//  0 hr  58'  30''
 import Head from 'next/head'
 import Image from 'next/image'
 
 // styles
-import styles from '../styles/home.module.css';
+import styles from '../styles/home.module.scss';
 
 
 
  
 export default function Home() {
   return (
-   <h1 className={styles.title}>Hello World</h1>
+   <h1 className={styles.title}><span>I'm a blue</span> Hello World</h1>
   )
 }
 
